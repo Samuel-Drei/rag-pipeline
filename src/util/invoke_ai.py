43b8355e8e72c15bc1654a0fc5cf24c3b4356594
Samuel Andrei <samuelandreit.teixeira@gmail.com>
2026-03-14 01:20:48 -0400

    Basics RAG configurations and KEYS are setted

--- src/util/invoke_ai.py
+++ src/util/invoke_ai.py
@@ -1,5 +1,8 @@
+import os
 from openai import OpenAI
+from dotenv import load_dotenv
 
+load_dotenv()  # lê o arquivo .env automaticamente
 
 def invoke_ai(system_message: str, user_message: str) -> str:
     """
@@ -7,9 +10,12 @@ def invoke_ai(system_message: str, user_message: str) -> str:
     Replace this if you want to use a different AI model.
     """
 
-    client = OpenAI()  # Insert the API key here, or use env variable $OPENAI_API_KEY.
+    client = OpenAI(
+        api_key=os.getenv("GEMINI_API_KEY"),
+        base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+    )
     response = client.chat.completions.create(
-        model="o4-mini",
+        model="gemini-2.0-flash",
         messages=[
             {"role": "system", "content": system_message},
             {"role": "user", "content": user_message},
